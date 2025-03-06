9725d58ababdd7f923fb2339d640b208fdd84706
ninanikolaevna <ninanikolaevna.t@gmail.com>
2025-03-06 12:22:16 +0300

    Введение в базы данных. without //

--- src/main/java/com/example/pro/sky/hogwarts/service/FacultyServiceImpl.java
+++ src/main/java/com/example/pro/sky/hogwarts/service/FacultyServiceImpl.java
@@ -50,30 +50,3 @@ public class FacultyServiceImpl implements FacultyService {
         facultyRepository.deleteById(id);
     }
 }
-//    private final HashMap<Long, Faculty> faculties = new HashMap<>();
-//    private long count = 0;
-//
-//    public Faculty addFaculty(Faculty faculty) {
-//        faculty.setId(count++);
-//        faculties.put(faculty.getId(), faculty);
-//        return faculty;
-//    }
-//
-//    public Faculty findFaculty(long id) {
-//        return null;
-//    }
-//
-//    public Faculty findFacultyById(long id) {
-//        return faculties.get(id);
-//    }
-//    public Faculty editFaculty(long id,Faculty faculty) {
-//        if(!faculties.containsKey(id)) {
-//            return null;
-//        }
-//        faculties.put(id, faculty);
-//        return faculty;
-//    }
-//   public void deleteFaculty (long id) {
-//        faculties.remove(id);
-//   }
-
